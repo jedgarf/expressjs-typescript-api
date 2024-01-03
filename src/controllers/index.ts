6401db294053ba91e77b3d7a74b0b0ef3dc851ac
John Edgar Francisco <jedgarf5@gmail.com>
2024-01-04 00:05:41 +0800

    add index in controllers

--- src/controllers/index.ts
+++
@@ -1,8 +0,0 @@
-import userControllers from './userControllers';
-
-const combinedControllers = {
-  users: userControllers,
-  // Add more route sections as needed
-};
-
-export default combinedControllers;
